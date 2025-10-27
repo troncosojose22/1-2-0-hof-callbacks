c549d9b7d15e6624d11015954e63d8bd49afa0e2
troncosojose22 <troncosojose22@gmail.com>
2025-10-27 12:06:21 -0400

    done

--- 1-callbacks.js
+++ 1-callbacks.js
@@ -12,12 +12,17 @@ greet('Ada');
 // --------------------------
 // Example 2: Higher-order functions that take in a callback function and a number as arguments.
 // --------------------------
+let count = 1;
+const incrementCount = () => {
+  console.log(count);
+  count++;
+}
 
 // Example 2a: setInterval
 const tick = () => {
   console.log("Tick");
 }
-setInterval(tick, 1000);
+setInterval(incrementCount, 1000);
 
 // Example 2b: setTimeout
 setTimeout(() => {
